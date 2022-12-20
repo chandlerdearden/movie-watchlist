5ee09af550ff6ce3e00025c03c7512fb519d51e5
Chandler Dearden <chandlerjdearden@gmail.com>
2022-12-20 11:50:28 -0700

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import Header from './components/Header';
+import MovieScreen from './components/MovieScreen';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [movieList, setMovieList] =  useState([])
+  const [watchList, setWatchList] = useState([])
+  const [page, setPage] = useState(1)
+
+  
+  const getData = () => {
+    axios
+        .get(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=${page}`)
+        .then((res) => {
+            console.log(res.data.results);
+            setMovieList(res.data.results);
+        });
+};
+
+  useEffect (() => {
+    getData()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <main>
+      <MovieScreen
+      movieList ={movieList}
+      page={page}
+      setPage={setPage}
+      list={watchList}/>
+      </main>
+      
     </div>
   );
 }
